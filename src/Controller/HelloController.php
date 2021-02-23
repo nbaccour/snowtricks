8866abb94258ce7581e21000f6fd96074c7e0094
nbaccour <46217602+nbaccour@users.noreply.github.com>
2021-02-23 22:46:43 +0100

    show page with route

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: msi-n
+ * Date: 23/02/2021
+ * Time: 18:39
+ */
+
+
+namespace App\Controller;
+
+
+class HelloController
+{
+
+
+    public function hello()
+    {
+
+        var_dump("Home page");
+        die;
+    }
+}
