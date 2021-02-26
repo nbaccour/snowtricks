f14c73e570bb7235c6d76517cfb9370e129599d9
nbaccour <46217602+nbaccour@users.noreply.github.com>
2021-02-26 23:45:18 +0100

    make user and create entity User

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,23 +4,50 @@ namespace App\DataFixtures;
 
 
 use App\Entity\Category;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
 class AppFixtures extends Fixture
 {
 
     protected $slugger;
-    public function __construct(SluggerInterface $slugger)
+    protected $encoder;
+
+    public function __construct(SluggerInterface $slugger, UserPasswordEncoderInterface $encoder)
     {
         $this->slugger = $slugger;
+        $this->encoder = $encoder;
     }
 
     public function load(ObjectManager $manager)
     {
 
         $faker = \Faker\Factory::create('FR-fr');
+
+        $admin = new User();
+        $hash = $this->encoder->encodePassword($admin, "password");
+        $admin->setEmail('admin@gmail.com')
+            ->setPassword($hash)
+            ->setPrenom('Nizar')
+            ->setNom('BACCOUR')
+            ->setRoles(['ROLE_ADMIN']);
+
+        $manager->persist($admin);
+
+        for ($u = 0; $u < 5; $u++) {
+            $user = new User();
+            $hash = $this->encoder->encodePassword($user, "password");
+            $user->setEmail('user' . $u . '@gmail.com')
+                ->setNom($faker->firstName())
+                ->setPrenom($faker->lastName())
+                ->setPassword($hash);
+
+            $manager->persist($user);
+        }
+
 
         $categoryName = ['Grabs', 'Rotations', 'Flips', 'Rotations désaxées', 'Slides', 'One foot'];
 
