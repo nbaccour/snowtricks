05d7129a6d6b5b07870a93529ded2324af4874ca
nbaccour <46217602+nbaccour@users.noreply.github.com>
2021-02-27 01:59:35 +0100

    add fom login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,17 +2,30 @@
 
 namespace App\Controller;
 
+use App\Form\LoginType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/login", name="security_login")
      */
-    public function login(): Response
+    public function login(AuthenticationUtils $utils): Response
     {
-        return $this->render('security/login.html.twig', []);
+
+        $form = $this->createForm(LoginType::class, ['email' => $utils->getLastUsername()]);
+        return $this->render('security/login.html.twig',
+            ['formView' => $form->createView(), 'error' => $utils->getLastAuthenticationError()]);
+    }
+
+    /**
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout()
+    {
+
     }
 }
--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -2,43 +2,76 @@
 
 namespace App\Security;
 
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\Exception\UsernameNotFoundException;
+use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Guard\AbstractGuardAuthenticator;
 
 class LoginFormAuthenticator extends AbstractGuardAuthenticator
 {
+    protected $encoder;
+
+    public function __construct(UserPasswordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function supports(Request $request)
     {
-        // todo
+
+        return $request->attributes->get('_route') === 'security_login'
+            && $request->isMethod('POST');
     }
 
     public function getCredentials(Request $request)
     {
-        // todo
+        return $request->request->get('login');//tableau avec 3 infos (email, pwd, _token)
     }
 
     public function getUser($credentials, UserProviderInterface $userProvider)
     {
-        // todo
+        try {
+            return $userProvider->loadUserByUsername($credentials['email']);//retourne l'utilisateur de la base de données
+        } catch (UsernameNotFoundException $e) {
+
+            throw new AuthenticationException("Cette adresse email n'est pas connue");
+        }
+
     }
 
     public function checkCredentials($credentials, UserInterface $user)
     {
-        // todo
+        // verifier que le mot de passe fourni, correspond au mot de passe de la base de données
+        $isValid = $this->encoder->isPasswordValid($user, $credentials['password']);
+
+        if (!$isValid) {
+            throw new AuthenticationException("Les informations de connexion sont invalides");
+        }
+        return true;
+//        return $this->encoder->isPasswordValid($user, $credentials['password']);
+
     }
 
     public function onAuthenticationFailure(Request $request, AuthenticationException $exception)
     {
-        // todo
+        $login = $request->request->get('login');
+        $request->attributes->set(Security::LAST_USERNAME, $login['email']);
+
+        $request->attributes->set(Security::AUTHENTICATION_ERROR, $exception);
+//        dd('erreur connexion');
     }
 
     public function onAuthenticationSuccess(Request $request, TokenInterface $token, string $providerKey)
     {
-        // todo
+//        dd('connexion valide');
+        //rediriger vers la page d'accueil
+        return new RedirectResponse('/');
     }
 
     public function start(Request $request, AuthenticationException $authException = null)
